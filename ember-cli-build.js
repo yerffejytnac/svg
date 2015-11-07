61ae93b9c43a77dc5135be657a54acfdecac762d
Jeffrey Canty <hello@jeffrey.engineer>
2015-11-07 17:45:33 -0600

    Add sass dependencies.
    Add ember-cli-sass, ember-cli-autoprefixer. Add configuration for
    autoprefixer. Rename app.css => app.scss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,6 +2,11 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {});
+  var app = new EmberApp(defaults, {
+    autoprefixer: {
+      browsers: ['last 2 versions'],
+      cascade: false
+    }
+  });
   return app.toTree();
 };
